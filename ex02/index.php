1e1db5914598d74d7bf78b71a347697897e746e7
Gustavo Mendes <gustavormendess@gmail.com>
2024-04-11 22:23:25 -0400

    date and hour in Brazil

--- ex02/index.php
+++ ex02/index.php
@@ -10,7 +10,9 @@
 <body>
   <h1>Exemplo de PHP</h1>
   <?php 
+    date_default_timezone_set("America/Porto_Velho");
     echo"Hoje é dia " . date("d/M/Y");
+    echo "<br>";
     echo "<br>";
     echo "E a hora atual é " . date("G:i:s");
   
